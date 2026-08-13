6c30d9be5709d74b2b3622ea2c3943b014d7aa41
Devendra687 <devsingh25873@gmail.com>
2026-08-14 02:46:26 +0530

    Add app launcher and screenshot support

--- jarvis.py
+++ jarvis.py
@@ -3,39 +3,64 @@ import webbrowser
 import pyttsx3
 import musicliab
 from google import genai
-
+from AppOpener import open
+import pyautogui
+from datetime import datetime
 
 recognizer = sr.Recognizer()
+
 
 def speak(text):
     engine = pyttsx3.init()
     engine.say(text)
     engine.runAndWait()
-    print(text) 
+    print(text)
+
 
 def aiprocess(command):
-    client = genai.Client(api_key="GEMINI_API_KEY")
+    client = genai.Client(
+        api_key=""
+    )
 
     response = client.models.generate_content(
-            model="gemini-3.1-flash-lite-preview",
-            contents=[
-                "You are a virtual assistant named Jarvis, skilled in general tasks like Alexa and Google Assistant. Keep answers short and helpful.",
-                command
-            ]
-        )
-    
+        model="gemini-3.1-flash-lite-preview",
+        contents=[
+            "You are a virtual assistant named Jarvis, skilled in general tasks like Alexa and Google Assistant. Keep answers short and helpful.",
+            command,
+        ],
+    )
+
     return response.text
 
 
+
+
 def processcommand(c):
-    if"open google" in c.lower():
+
+    # for yt search
+    if c.lower().startswith("search"):
+        query = c.lower().replace("search", " ").strip()
+        url = "https://www.youtube.com/results?search_query=" + query.replace(" ", "+")
+        webbrowser.open(url)
+    # for screenshot
+    elif "take a screenshot" in c.lower():
+        name = datetime.now().strftime("screenshot_%Y%m%d_%H%M%S.png")
+        pyautogui.screenshot(name)
+        speak("Screenshot taken sir")
+        # open websites
+    elif "open google" in c.lower():
         webbrowser.open("https://google.com")
-    elif"open youtube" in c.lower():
+    elif "open youtube" in c.lower():
         webbrowser.open("https://youtube.com")
-    elif"open insta" in c.lower():
+    elif "open insta" in c.lower():
         webbrowser.open("https://instagram.com")
-    elif"open snap" in c.lower():
+    elif "open snap" in c.lower():
         webbrowser.open("https://snapchat.com")
+        # app opening
+    elif c.lower().startswith("open"):
+        app = c.lower().replace("open", " ").strip()
+        open(app)
+        # for music
     elif c.lower().startswith("play"):
         song = c.lower().replace("play", " ").strip()
         if song in musicliab.music:
@@ -45,38 +70,35 @@ def processcommand(c):
         else:
             speak("Song not found")
     else:
-        #let open ai handel the request
-        output=aiprocess(c)
+        # let open ai handel the request
+        output = aiprocess(c)
         speak(output)
-        pass
+
 
 if __name__ == "__main__":
     speak("Initializing Jarvis....")
     while True:
-        #listen for the wake word "jarvis"
+        # listen for the wake word "jarvis"
         r = sr.Recognizer()
-
 
         print("recoginzing....")
         # recognize speech using google
         try:
             with sr.Microphone() as source:
                 print("Say something!")
-                r=recognizer
+                r = recognizer
                 r.adjust_for_ambient_noise(source, duration=1)
-                audio = r.listen(source,timeout=3,phrase_time_limit=2)
-            word=r.recognize_google(audio).lower().strip()
+                audio = r.listen(source, timeout=3, phrase_time_limit=3)
+            word = r.recognize_google(audio).lower().strip()
             print("Heard:", word)
-            if(word.lower() == "jarvis"):
+            if word.lower() == "jarvis":
                 speak("yess boss")
-                #listen for command
+                # listen for command
                 with sr.Microphone() as source:
-                    # print("jarvis active....!")
-                    speak("jarvis activated")
                     print(word)
                     r.adjust_for_ambient_noise(source, duration=1)
                     audio = r.listen(source)
-                    command=r.recognize_google(audio)
+                    command = r.recognize_google(audio)
                     print("Command:", command)
                     processcommand(command)
         except sr.UnknownValueError:
--- musicliab.py
+++ musicliab.py
@@ -2,5 +2,6 @@ music = {
     "dogs and snake":"https://music.youtube.com/watch?v=Psp-1X_au5w",
     "champion":"https://music.youtube.com/watch?v=WGlu7qJCOIk&list=RDAMVMPsp-1X_au5w",
     "azadi":"https://music.youtube.com/watch?v=hSyxwSEOxqs&list=RDAMVMPsp-1X_au5w",
-    "blue":"https://music.youtube.com/watch?v=MHCsrKA9gh8"
+    "blue":"https://music.youtube.com/watch?v=MHCsrKA9gh8",
+    "i thought i saw":"https://music.youtube.com/watch?v=HJstyRBLqBQ&list=RDAMVMHJstyRBLqBQ",
 }
